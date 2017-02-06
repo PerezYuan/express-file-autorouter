dfeb10987c66f9164caa0b8c83aee2d8b407aafe
PerezYuan <yzmspirit@sina.cn>
2017-02-06 23:44:14 +0800

    check dir

--- autoRouter.test.js
+++ autoRouter.test.js
@@ -2,7 +2,6 @@ var AutoRouter = require('./index');
 var path = require('path');
 var app = require('express')();
 
-console.log(path.resolve(__dirname, 'routerPath'));
 var autoRouter = new AutoRouter({
     routerPath: path.resolve(__dirname, 'routerPath'), //存放路由的path
     app: app, //express app
