769e872d79eb5b1c3159a1929e440e7889c2702e
perezyuan <yzmspirit@sina.cn>
2017-02-07 11:10:00 +0800

    v1.0.0

--- autoRouter.test.js
+++ autoRouter.test.js
@@ -5,7 +5,13 @@ var app = require('express')();
 var autoRouter = new AutoRouter({
     routerPath: path.resolve(__dirname, 'routerPath'), //存放路由的path
     app: app, //express app
-    alias: {}
+    alias: {
+        '/router1' : '/change1'
+    }
 });
 
-//console.log(Object.keys(autoRouter.getRouters()));
+console.log(Object.keys(autoRouter.getRouters()));
+
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!');
+});
--- routerPath/router2.js
+++ routerPath/router2.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', (req, res, next) => {
-    res.send('route1');
+    res.send('route2');
 });
 
 module.exports = router;
